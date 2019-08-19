03090cff5ad136fb129458914f347a2bf62738da
Thomas Katalenas <noitrix@Thomass-MacBook-Air.local>
2019-08-19 17:16:22 -0400

    added map functionality

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -44,3 +44,23 @@ console.log(subtract(1, 2))
 //   return num * 3;
 // });
 // console.log(triple);
+
+var array1 = [{key1: 1, key2: 4, key3: 9, key4: 16},
+              {key1: 3, key2: 6, key3: 10, key4: 22},
+              {key1: 5, key2: 14, key3: 56, key4: 18}];
+
+// pass a function to map
+const map1 = array1.map(x => x.key1 * 2);
+
+console.log(map1);
+
+var list = [1,2,3,4];
+const map2 = array1.map(function(num){
+    return [num.key1 * 4,  num.key2 +2,  num.key3 - 3,  num.key4 /2];
+})
+console.log(map2);
+
+const map3 = array1.map(function(num){
+    return [{key1: num.key1 * 4, key2: num.key2 +2, key3: num.key3 - 3, key4: num.key4 /2}];
+})
+console.log(map3);
